403506d8dbfbb4122b366aac13b754463735c16f
TheCatWest <catwestover@outlook.com>
2021-06-06 14:07:02 -0700

    add Tag and Product Tag table columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,23 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      model: 'Tag',
+      key: 'id'
+    }
   },
   {
     sequelize,
